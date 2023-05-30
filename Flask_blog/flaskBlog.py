c898a84ce95c1cf73211de0d898847380dcbf2a2
Jesse Mucheke <jmucheke274@gmail.com>
2023-05-30 08:24:57 +0300

    Setting up the flask environment and starting of with flask routes

---
+++ Flask_blog/flaskBlog.py
@@ -0,0 +1,31 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+posts = [
+ {
+  'author':"Jesse Mucheke",
+  'title':"The day that rains",
+  'content':"My first content",
+  'date_posted':"April 20, 2023"
+ },
+ {
+  'author':"Jessica Kadzo",
+  'title':"I am God's son",
+  'content':"My second content",
+  'date_posted':"April 22, 2023"
+ }
+]
+
+
+@app.route("/")
+@app.route("/home")
+def home():
+    return render_template("home.html", posts=posts)
+
+@app.route("/about")
+def about():
+ return render_template("about.html")
+
+if __name__ == '__main__':
+ app.run(debug=True)
